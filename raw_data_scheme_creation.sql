cf94135453afab27a3cbe9d847480283918c5f20
Paola Orjuela <138379593+paorjuela@users.noreply.github.com>
2026-04-26 19:16:37 -0600

    Refactor raw_data_scheme_creation.sql for schema creation

--- raw_data_scheme_creation.sql
+++ raw_data_scheme_creation.sql
@@ -1,11 +1,9 @@
---\copy orders from 'C:/Users/paola/Downloads/archive/Global_Superstore2.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',');
+DROP SCHEMA IF EXISTS raw CASCADE;
+CREATE SCHEMA IF NOT EXISTS raw;
 
-DROP TABLE IF EXISTS orders;
-DROP TABLE IF EXISTS clientes;
-DROP TABLE IF EXISTS productos;
-DROP TABLE IF EXISTS ubicaciones;
+DROP TABLE IF EXISTS raw.orders;
 
-CREATE TABLE orders (
+CREATE TABLE raw.orders (
     row_id INT PRIMARY KEY,
     order_id VARCHAR(50),
     order_date DATE,
@@ -31,63 +29,6 @@ CREATE TABLE orders (
     shipping_cost NUMERIC(15,2),
     order_priority VARCHAR(20)
 );
-SELECT *
-FROM orders;
 
---------------------------------------------------------------------------
---sí funciona
---crea tabla clientes
-START TRANSACTION;
-    CREATE TABLE clientes AS
-        SELECT DISTINCT customer_id, customer_name, segment
-        FROM orders;
-
-    ALTER TABLE clientes ADD PRIMARY KEY (customer_id);
-
-    SELECT *
-    FROM clientes;
---ROLLBACK;
-COMMIT;
-
-SELECT *
-FROM clientes;
---------------------------------------------------------------------------------------------------
---funciona crear la tabla productos
---PERO no funciona crear la llave primaria porque hay duplicados en el id
-
-START TRANSACTION;
-    CREATE TABLE productos AS
-        SELECT DISTINCT product_id, product_name, category, sub_category
-        FROM orders;
-
-    select *
-        from productos;
-
-    ALTER TABLE productos ADD PRIMARY KEY (product_id); --no funciona
-commit;
-
-select *
-from productos
-where product_id = 'OFF-AR-10000751'; --lol q mal. hay que arreglar eso y despues la llave primaria
-
-------------------------------------------------------------
---funciona crear la tabla ubicaciones
-START TRANSACTION;
-    CREATE TABLE ubicaciones (
-        id_ubicacion SERIAL PRIMARY KEY,
-        city VARCHAR(100),
-        state VARCHAR(100),
-        country VARCHAR(100),
-        postal_code VARCHAR(20),
-        market VARCHAR(25),
-        region VARCHAR(25)
-    );
-
-    INSERT INTO ubicaciones (city, state, country, postal_code, market, region)
-    SELECT DISTINCT city, state, country, postal_code, market, region
-    FROM orders;
-
-    select *
-        from ubicaciones;
-commit ;
+\COPY raw.orders (row_id, order_id, order_date, ship_date, ship_mode, customer_id, customer_name, segment, city, state, country, postal_code, market, region, product_id, category, sub_category, product_name, sales, quantity, discount, profit, shipping_cost, order_priority) FROM 'C:/Global_Superstore2.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',');
 
